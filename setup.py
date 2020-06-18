07cbb3f0d9297127d36cf0a8b8f120007c867ecf
Thomas <thomas.rieutord@gmail.com>
2020-06-18 23:36:41 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kabl",
-    version="0.0.1",
+    version="2.0.0",
     author="Meteo-France",
     author_email="thomas.rieutord@meteo.fr",
     description="BL estimation from lidar",
